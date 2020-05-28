7f74a876727f16a9de72e24c225f45770190569b
minamihiroto <kokowatamf@gmail.com>
2020-05-29 00:32:04 +0900

    ルート追加

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::get('/top', 'PostsController@index')->name('top')->middleware('auth');
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show','destroy']]);
+Route::resource('comments', 'CommentsController', ['only' => ['store']]);
