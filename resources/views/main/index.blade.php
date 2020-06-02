b6fc129c30972cce7466bbd3e79824b64ef9395b
minamihiroto <kokowatamf@gmail.com>
2020-06-02 14:35:34 +0900

    topでnews５件を見れるよう変更

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.main')
 @section('title','Top')
 @section('content')
-  this is top-page!
+
+  <section class="news">
+      <h1>news</h1>
+    @if(count($items) > 0)
+      @foreach($items as $item)
+          <div class="announce" role="alert">
+            <a href="/news/{{$item->id}}">{{ $item->news_title }}</a>
+          </div>
+      @endforeach
+      <a href="/news">以前のお知らせを見る ＞＞</a>
+    @else
+      <div>投稿記事がありません</div>
+    @endif
+  </section>
+
 @endsection
