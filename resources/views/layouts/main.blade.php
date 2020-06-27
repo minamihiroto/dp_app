3ee94eeb760710aa1ee0eb1ddc36183265edebb2
minamihiroto <kokowatamf@gmail.com>
2020-06-27 10:34:25 +0900

    トップページ背景変更

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,7 +21,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="/#reserve">予約</a>
+          <a class="nav-link" href="https://airrsv.net/dearpilates/calendar">予約</a>
         </li>
 
         <li class="nav-item">
