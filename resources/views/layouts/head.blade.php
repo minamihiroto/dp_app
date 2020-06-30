75955d0fd24ba7bc7820255e6af942994601e4aa
minamihiroto <kokowatamf@gmail.com>
2020-06-30 16:49:02 +0900

    サブタイトル追加

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -8,7 +8,7 @@
   <meta name="description" content="ピラティスを中心としたエクササイズを光明池で、気軽に生活に取り入れることができます。人気急上昇中のエクササイズを大阪で体験していただけます。" />
   <link rel="shortcut icon" href="{{asset('/images/favicon.ico')}}">
   @yield('css')
-  <title>Dear Pilates</title>
+  <title>泉北光明池のピラティススタジオ | Dear Pilates</title>
 </head>
 <body class="@yield('body_class')">
   @yield('header')
