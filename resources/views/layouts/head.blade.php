b2e036db810254a79567ecac563eb6a124e9695c
minamihiroto <kokowatamf@gmail.com>
2020-06-30 15:00:40 +0900

    メタタグ変更

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,9 +4,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="Keywords" content="ピラティス,ぴらてぃす,ヨガ,ダイエット,大阪,光明池,和泉,エクササイズ,ディアピラティス,でぃあぴらてぃす,dearpilates" />
+  <meta name="description" content="ピラティスを中心としたエクササイズを光明池で、気軽に生活に取り入れることができます。人気急上昇中のエクササイズを大阪で体験していただけます。" />
   <link rel="shortcut icon" href="{{asset('/images/favicon.ico')}}">
   @yield('css')
-  <title>DearPilates</title>
+  <title>Dear Pilates</title>
 </head>
 <body class="@yield('body_class')">
   @yield('header')
